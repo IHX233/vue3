3eca7a536e53e29cbf845b9cc4c1d0187785d61f
ihx <2944177547@qq.com>
2023-01-05 22:41:04 +0800

    setup

---
+++ test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>{{ name }}</div>
+  <div>{{ age }}</div>
+  <div @click="changeInfo">click</div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: 'App',
+  setup(){
+    let name = ref('ihx')
+    let age = ref(18)
+    function changeInfo(){
+      name.value = 'gf'
+      age.value = 24
+    }
+    return{name,age,changeInfo}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
