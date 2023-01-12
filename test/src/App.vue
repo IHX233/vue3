5e741372e5961bc425f4b6efad21405fd2bcc1f4
ihx <2944177547@qq.com>
2023-01-12 23:18:46 +0800

    toRef

--- test/src/App.vue
+++ test/src/App.vue
@@ -2,15 +2,17 @@
   <demo :title="'vue3'" @hello="hello"></demo>
   <computed></computed>
   <hook></hook>
+  <to-ref></to-ref>
 </template>
 
 <script>
 import Demo from './components/Demo'
 import Computed from './components/Computed'
 import Hook from './components/Hook'
+import ToRef from './components/ToRef.vue'
 export default {
   name: 'App',
-  components:{Demo,Computed,Hook},
+  components:{Demo,Computed,Hook,ToRef},
   setup(){
     function hello(value){
       alert(value)
---
+++ test/src/components/ToRef.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>{{name}}</div>
+    <div>{{workType}}</div>
+    <div>{{workName}}</div>
+  </template>
+  
+  <script>
+  import {reactive, computed, toRef, toRefs} from 'vue'
+  export default {
+    name: 'ToRef',
+    setup(){
+      let person = reactive({
+        name:'gf',
+        work:{
+            type:'it',
+            name:'qd'
+        }
+      })
+      
+      return{
+        person,
+        workType:toRef(person.work,'type'),
+        workName:toRef(person.work,'name'),
+        ...toRefs(person)
+
+    }
+    }
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
