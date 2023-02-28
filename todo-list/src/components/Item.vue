e8451703aeab9b09ef551cfc7db9a285010081c4
ihx <2944177547@qq.com>
2023-02-28 23:07:59 +0800

    todolist init

---
+++ todo-list/src/components/Item.vue
@@ -0,0 +1,45 @@
+<template>
+    <li>
+        <label>
+            <input type="checkbox">
+            <span></span>
+        </label>
+        <button class="btn btn-danger" style="display:none"></button>
+    </li>
+</template>
+<script>
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name:'Item'
+  })
+</script>
+<style scoped>
+    li{
+        list-style: none;
+        height:36px;
+        line-height: 36px;
+        padding:0 5px;
+        border-bottom:1px solid #ddd;
+    }
+    li label{
+        float:left;
+        cursor:pointer;
+    }
+    li label li input{
+        vertical-align: middle;
+        margin-right:6px;
+        position:relative;
+        top:-1px;
+    }
+    li button{
+        float:right;
+        display: none;
+        margin-top:3px;
+    }
+    li:last-child{
+        border-bottom: none;
+    }
+    li::before{
+        content:initial;
+    }
+</style>
